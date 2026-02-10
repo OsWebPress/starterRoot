25c75e500d854c0306c3d6ddf8fed3e7149131f6
obult <oswin@pixelcorp.nl>
2026-02-10 21:29:07 +0100

    add pop out example

---
+++ component/popOut.vue
@@ -0,0 +1,7 @@
+<!-- Example component how to reverse the left marging to make a full width container and still respect the vertical spacing -->
+
+<template>
+  <div class="w-screen -ml-24 bg-gray-100 box-border">
+    <slot></slot>
+  </div>
+</template>
